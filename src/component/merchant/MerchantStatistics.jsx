1e9c3093a368f6ef4f7278a3ca84bfd9df67856a
levent8421 <levent8421@outlook.com>
2020-09-09 15:57:20 +0800

    edit merchant statistics

--- src/component/merchant/MerchantStatistics.jsx
+++ src/component/merchant/MerchantStatistics.jsx
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
+import {mapStateAndActions} from '../../store/storeUtils';
+import './MerchantStatistics.less';
+import {Alert} from 'antd';
 
 class MerchantStatistics extends Component {
+    componentDidMount() {
+        this.props.setTitle('商户统计分析', '入驻商户统计分析');
+    }
+
     render() {
         return (
-            <div>
+            <div className="merchant-statistics">
                 Merchant Statistics
+                <Alert type="success" message="开发中"/>
             </div>
         );
     }
 }
 
-export default MerchantStatistics;
+export default mapStateAndActions(MerchantStatistics);
